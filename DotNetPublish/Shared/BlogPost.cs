387f1ae8bf9318dd65be92b618285a860a415501
Bill Nalen <bill@nalens.com>
2023-01-14 16:49:31 -0700

    Started adding client side model and components

---
+++ DotNetPublish/Shared/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetPublish.Shared;
+
+public class BlogPost
+{
+    public int Id { get; set; }
+    public string Slug { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime DatePublished { get; set; } = DateTime.Now;
+    public bool IsPublished { get; set; } = false;
+    public bool IsDeleted { get; set; } = false;
+}
